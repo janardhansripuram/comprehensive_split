fbd43d9f41ddb4ee601991190949363bb1472d7c
janardhansripuram <janardhansripuram@gmail.com>
2025-07-16 00:07:17 +0800

    Updated firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from '@firebase/storage';
 
 // Firebase configuration - Replace with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:xxxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyC-y4YY70SOfqFu5fdEn6KQP1iil1Ggutg",
+  authDomain: "oweme-469e0.firebaseapp.com",
+  projectId: "oweme-469e0",
+  storageBucket: "oweme-469e0.appspot.com",
+  messagingSenderId: "736723332834",
+  appId: "1:736723332834:android:4d7b077515d8d99544f8bb"
 };
 
 // Initialize Firebase
